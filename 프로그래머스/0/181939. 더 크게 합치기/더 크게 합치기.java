b61480d8f2eb2892bf400a860fd3bd8d30ce03d5
CHU DUC DIEM <112887374+diemchu@users.noreply.github.com>
2024-07-23 02:03:35 +0900

    [level 0] Title: 더 크게 합치기, Time: 9.79 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,7 +1,7 @@
 class Solution {
      public int solution(int a, int b) {
-        int ab = Integer.parseInt(new StringBuilder().append(a).append(b).toString());
-        int ba = Integer.parseInt(new StringBuilder().append(b).append(a).toString());
+       int ab = Integer.parseInt(""+a+b);
+        int ba = Integer.parseInt(""+b+a);
         return  ab > ba ? ab : ba;
     }
 }
