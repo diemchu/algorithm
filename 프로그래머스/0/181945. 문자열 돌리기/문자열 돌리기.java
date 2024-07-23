a58cb668a11d9e0dcce1a042ef991b9c4ab010f9
CHU DUC DIEM <112887374+diemchu@users.noreply.github.com>
2024-07-23 15:18:53 +0900

    [level 0] Title: 문자열 돌리기, Time: 185.92 ms, Memory: 65.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+        String a =sc.next();
+        a.chars().forEach(v-> System.out.println((char)v));
+    }
+}
