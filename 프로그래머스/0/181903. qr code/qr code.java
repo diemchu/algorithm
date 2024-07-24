b0ce0db67cbfd0c00059035d8f996a97068a1e07
CHU DUC DIEM <112887374+diemchu@users.noreply.github.com>
2024-07-24 16:42:36 +0900

    [level 0] Title: qr code, Time: 0.13 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -1,9 +1,8 @@
 class Solution {
-       public String solution(int q, int r, String code) {
+ public String solution(int q, int r, String code) {
             StringBuilder s = new StringBuilder();
-            char [] chars = code.toCharArray();
-            for(int i = 0 ; i < chars.length ;i++){
-                if(i%q == r) s.append(chars[i]);
+            for(int i = r ; i < code.length() ;i+=q){
+                s.append(code.charAt(i));
             }
             return  s.toString();
         }
